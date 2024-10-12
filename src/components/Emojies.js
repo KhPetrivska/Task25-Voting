dcb7b0db4aef12315b0e97a3599384e7aa158e78
Kristina <kristin.petrivska@gmail.com>
2024-10-11 22:35:45 -0600

    provide code optimization

--- src/components/Emojies.js
+++ src/components/Emojies.js
@@ -11,30 +11,23 @@ import "./emojiesStyles.css";
 export default class Emojies extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count:
-      {
-        angry: 0,
-        elated: 0,
-        french: 0,
-        happy: 0,
-        sherlock: 0,
-      } 
-    };
-
-  }
-
-  componentDidMount() {
     const savedCounts = localStorage.getItem("emojiCounter");
-    if (savedCounts) {
-      this.setState({ count: JSON.parse(savedCounts) });
-    }
+    const count = savedCounts
+      ? JSON.parse(savedCounts)
+      : {
+          angry: 0,
+          elated: 0,
+          french: 0,
+          happy: 0,
+          sherlock: 0,
+        };
+    this.state = { count };
   }
 
   onClick = (emoji) => {
-    this.setState((prevState) => ({
-        count: {...prevState.count,[emoji]: prevState.count[emoji] + 1,
-        },
+    this.setState(
+      (prevState) => ({
+        count: { ...prevState.count, [emoji]: prevState.count[emoji] + 1 },
       }),
       () => {
         localStorage.setItem("emojiCounter", JSON.stringify(this.state.count));
@@ -42,60 +35,56 @@ export default class Emojies extends React.Component {
     );
   };
   clearResults = () => {
-    this.state = {
-      count:
-      {
-        angry: 0,
-        elated: 0,
-        french: 0,
-        happy: 0,
-        sherlock:0
-      }
-    }
-    localStorage.setItem("emojiCounter", JSON.stringify(this.state.count));
-    this.componentDidMount()
-    
+    const newCount = {
+      angry: 0,
+      elated: 0,
+      french: 0,
+      happy: 0,
+      sherlock: 0,
+    };
+    this.setState({ count: newCount }, () => {
+      localStorage.setItem("emojiCounter", JSON.stringify(newCount));
+    });
   };
 
-
   render() {
+    const smiles = [
+      { name: "angry", src: angryEm },
+      { name: "elated", src: elatedEm },
+      { name: "french", src: frenchEm },
+      { name: "happy", src: happyEm },
+      { name: "sherlock", src: sherlockEm },
+    ];
+    this.state = {
+      count: smiles.reduce((prevCount, { name }) => {
+        prevCount[name] = 0;
+        return prevCount;
+      }, {}),
+    };
+
     return (
       <>
-      <div className="em-container">
-        <div className="em-counter-img">
-          <img onClick={()=>this.onClick('angry')} className="em-img" src={angryEm} />
-          <span className="em-counter">
-            <p>{this.state.count.angry}</p>
-          </span>
+        <div className="em-container">
+          {smiles.map(({ name, src }) => {
+            return (
+              <div key={name} className="em-counter-img">
+                <img
+                  onClick={() => this.onClick(name)}
+                  className="em-img"
+                  src={src}
+                />
+                <span className="em-counter">
+                  <p>{this.state.count[name]}</p>
+                </span>
+              </div>
+            );
+          })}
         </div>
-        <div className="em-counter-img">
-          <img onClick={()=>this.onClick('elated')} className="em-img" src={elatedEm} />
-          <span className="em-counter">
-          <p>{this.state.count.elated}</p>
-          </span>
+        <div className="secondary-button-container">
+          <button className="secondary button" onClick={this.clearResults}>
+            Clear results
+          </button>
         </div>
-        <div className="em-counter-img">
-          <img onClick={()=>this.onClick('french')}  className="em-img" src={frenchEm} />
-          <span className="em-counter">
-          <p>{this.state.count.french}</p>
-          </span>
-        </div>
-        <div className="em-counter-img">
-          <img onClick={()=>this.onClick('happy')}  className="em-img" src={happyEm} />
-          <span className="em-counter">
-          <p>{this.state.count.happy}</p>
-          </span>
-        </div>
-        <div className="em-counter-img">
-          <img onClick={()=>this.onClick('sherlock')}  className="em-img" src={sherlockEm} />
-          <span className="em-counter">
-          <p>{this.state.count.sherlock}</p>
-          </span>
-        </div>
-        </div>
-        <div className='secondary-button-container'>
-    <button className='secondary button' onClick={this.clearResults}>Clear results</button>
-    </div>
       </>
     );
   }
